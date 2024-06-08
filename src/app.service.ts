bd4cbe6eddb0c6dea9da82e16fcf602b2254dd4e
mthPiltz <matheus.piltz@ufms.br>
2024-06-08 15:14:37 -0400

    retornando top artistas

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  
+  constructor(
+    private readonly httpService: HttpService) {
+  }
+
+  async getTopArtists(access_token : string) {
+    const url = 'https://api.spotify.com/v1/me/top/artists?limit=10&offset=0'
+    const headers = {
+      'Authorization': 'Bearer ' + access_token
+    } 
+
+    const response = await firstValueFrom(
+      this.httpService.get(url, { headers }));
+    
+    return response.data
   }
 }
